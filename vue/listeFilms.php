98b2b3a50ea16b595fac80382e0a626d56476c13
Seboss666 <sebastien_verdet@seboss666.info>
2019-05-06 13:21:56 +0200

    Add year of film to list

--- vue/listeFilms.php
+++ vue/listeFilms.php
@@ -1,13 +1,14 @@
 <?php ob_start() ?>
 <ul data-role="listview" data-inset="false" data-icon="false" data-divider-theme="b">
 <li data-role="list-divider"><span class="ui-li-count"><?php echo $counts; ?> Résultat<?php if ($counts > 1) { echo "s"; } ?></span></li>
-<?php foreach ($films as $film): 
+<?php foreach ($films as $film):
   ?>
   <li>
     <a href="<?php echo $lienFilm . $film['id']; ?>"><h2><?php echo $film['titre']; ?></h2>
-<?php if ($film['pret']) { ?>
-  <p>Prété à <?php echo $film['nom_pret']; ?></p>
-<?php } ?>
+    <p>
+<?php $prete=""; if ($film['pret']) {
+  $prete=" - Prété à " . $film['nom_pret']; } echo $film['annee'] . $prete; ?></p>
+
   <p class="ui-li-aside"><?php echo date("d/m/Y", strtotime($film['dateAjout'])); ?></p>
     </a>
   </li>
